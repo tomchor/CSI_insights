cb6dad87eaa40eccfbb3b6b442620c3520d5312f
tomchor <tomaschor@gmail.com>
2021-03-15 18:59:15 -0600

    increased time limit for running interior jet

--- run_intjet_2dnp.sh
+++ run_intjet_2dnp.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=i2CIjet01
 #SBATCH --output=out/slurm.out
 #SBATCH --error=out/%x.err
-#SBATCH --time=8:00:00
+#SBATCH --time=16:00:00
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:v100:1 # GPUs per Node
 #SBATCH --mail-user=tchor@umd.edu
