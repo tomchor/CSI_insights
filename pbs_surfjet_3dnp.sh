e0e7b1f797780cabd30717712c9406fca434b2e6
tomchor <tomaschor@gmail.com>
2021-03-31 13:00:34 -0600

    organized up slurm and pbs scripts for all simulations

---
+++ pbs_surfjet_3dnp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#PBS -A UMCP0012
+#PBS -N sj3dSIjet4
+#PBS -k eod
+#PBS -o logs/sj3d_SIjet4.out
+#PBS -e logs/sj3d_SIjet4.err
+#PBS -l walltime=24:00:00
+#PBS -q casper
+#PBS -l select=1:ncpus=1:ngpus=1
+#PBS -l gpu_type=v100
+#PBS -M tchor@umd.edu
+#PBS -m abe
+
+# Clear the environment from any previously loaded modules
+module purge
+module load gnu
+module load cuda
+
+#/glade/u/apps/ch/opt/usr/bin/dumpenv # Dumps environment (for debugging with CISL support)
+
+/glade/u/home/tomasc/repos/julia/julia --project \
+    surfjet_3dnp.jl --jet=SIjet4 --arch=GPU --factor=4 2>&1 | tee out/sj3d_SIjet4.out
+
---
+++ slurm_intjet_2dnp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH --account=UMCP0012
+#SBATCH --job-name=i2CIjet01
+#SBATCH --output=out/slurm.out
+#SBATCH --error=out/%x.err
+#SBATCH --time=16:00:00
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:v100:1 # GPUs per Node
+#SBATCH --mail-user=tchor@umd.edu
+#SBATCH --mail-type=ALL
+
+# Clear the environment from any previously loaded modules
+module purge
+module load gnu
+module load cuda
+
+/glade/u/apps/ch/opt/usr/bin/dumpenv # Dumps environment (for debugging with CISL support)
+
+/glade/u/home/tomasc/repos/julia/julia --project \
+    intjet_2dnp.jl --jet=CIjet01 --arch=GPU --factor=1 2>&1 | tee out/ij2d_CIjet01.out
+
