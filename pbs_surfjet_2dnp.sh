2eed672e0855a0510ab29665d89efbe330195bc7
tomchor <tomaschor@gmail.com>
2021-04-08 12:54:57 -0600

    changed jet set-up for surf2d run

--- pbs_surfjet_2dnp.sh
+++ pbs_surfjet_2dnp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #PBS -A UMCP0012
-#PBS -N sj2dCIjet1
+#PBS -N sj2dSIjet4
 #PBS -k eod
-#PBS -o logs/sj2d_CIjet1.out
-#PBS -e logs/sj2d_CIjet1.err
+#PBS -o logs/sj2d_SIjet4.out
+#PBS -e logs/sj2d_SIjet4.err
 #PBS -l walltime=16:00:00
 #PBS -q casper
 #PBS -l select=1:ncpus=1:ngpus=1
@@ -19,5 +19,5 @@ module load cuda
 #/glade/u/apps/ch/opt/usr/bin/dumpenv # Dumps environment (for debugging with CISL support)
 
 /glade/u/home/tomasc/repos/julia_1.5.2/julia --project \
-    surfjet_2dnp.jl --jet=CIjet1 --arch=GPU --factor=1 2>&1 | tee out/sj2d_CIjet1.out
+    surfjet_2dnp.jl --jet=SIjet4 --arch=GPU --factor=1 2>&1 | tee out/sj2d_SIjet4.out
 
