4d2caa9391e6c47010d04fcd408c88a95bc616f0
tomchor <tomaschor@gmail.com>
2021-04-26 09:16:54 -0600

    added unified scripts to run and launch 2d and 3d jets

---
+++ pbs_surfjet_np.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#PBS -A UMCP0012
+#PBS -N S2d_SIjet4
+#PBS -k eod
+#PBS -o logs/S2d_SIjet4.out
+#PBS -e logs/S2d_SIjet4.err
+#PBS -l walltime=24:00:00
+#PBS -q casper
+#PBS -l select=1:ncpus=1:ngpus=1
+#PBS -l gpu_type=v100
+#PBS -M tchor@umd.edu
+#PBS -m abe
+
+# Clear the environment from any previously loaded modules
+module purge
+module load gnu
+module load cuda/11.0.3
+
+#/glade/u/apps/ch/opt/usr/bin/dumpenv # Dumps environment (for debugging with CISL support)
+
+/glade/u/home/tomasc/repos/julia_1.5.2/julia --project \
+    surfjet_2dnp.jl --jet=SIjet4 --arch=GPU --factor=1 2>&1 | tee out/S2d_SIjet4.out
+
